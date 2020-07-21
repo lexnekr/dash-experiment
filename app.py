e6a65064c9a2a85c040432a84bb0c85a375c2e61
lexnekr <lexnekr@gmail.com>
2020-07-21 18:44:51 +0300

    Dash Hello World

--- app.py
+++ app.py
@@ -1,49 +1,29 @@
-import plotly.graph_objs as go
-import numpy as np
-import dash_auth
+import os
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-x = np.arange(0, 5, 0.1)
-def f(x):
-    return x**2
-def h(x):
-    return np.sin(x)
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-fig1 = go.Figure(go.Scatter(x=x, y=f(x)))
-fig2 = go.Figure(go.Scatter(x=x, y=h(x)))
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
-external_stylesheets = ['https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css']
+server = app.server
 
-app = dash.Dash(external_stylesheets=external_stylesheets)
-auth = dash_auth.BasicAuth(
-    app,
-    [('admin', 'secret')]
-)
+app.layout = html.Div([
+    html.H2('Hello World'),
+    dcc.Dropdown(
+        id='dropdown',
+        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+        value='LA'
+    ),
+    html.Div(id='display-value')
+])
 
-row0 = html.Div([html.Div([html.H1('Заголовок')], 
-                          className='col-sm')],
-                className='row')
+@app.callback(dash.dependencies.Output('display-value', 'children'),
+              [dash.dependencies.Input('dropdown', 'value')])
+def display_value(value):
+    return 'You have selected "{}"'.format(value)
 
-row1 = html.Div([html.Div([dcc.Graph(figure=fig1)], 
-                          className='col-sm'),
-                 html.Div([dcc.Graph(figure=fig2)], 
-                          className='col-sm')], 
-                className='row')
-
-row2 = html.Div([html.Div([html.P('f(x) = x<sup>2</sup>')], 
-                          className='col-sm'),
-                 html.Div([html.P('h(x) = sin(x)')], 
-                          className='col-sm')], 
-                className='row')
-
-app.layout = html.Div([row0, row1, row2], 
-                      className='container')
-
-
-
-
-#app.run()
 if __name__ == '__main__':
-    app.run_server(debug=True, use_reloader=True)  # Turn off reloader if inside Jupyter
+    app.run_server(debug=True)
